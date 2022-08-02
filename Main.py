a334d84bdc5d2a1bab9da37e7ed4a9ba18cd26de
yuvraj420190 <108419015+yuvraj420190@users.noreply.github.com>
2022-08-02 22:02:30 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,26 +8,25 @@ class Stack:
         # Write code here
         if len(self.item)==0 :
             return True
-
-    def is_full(self):
+       
+     def is_full(self):
         # Write code here
         if len(self.item)==self.size :
             return True
-
-    def push(self, data):
+            
+     def push(self, data):
         if not self.is_full():
             self.item.append(data)
 
-    def pop(self):
+     def pop(self):
         if not self.is_empty():
             self.item.pop()
 
-    def status(self):
+     def status(self):
         if not self.is_empty():
             for i in self.item:
                 print(i)
-
-
+                
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
 stack = Stack(size)
